9b5ba6509c6ec0977669e242fbc4245baec78783
William Fitzgerald <97709175+fitz-1@users.noreply.github.com>
2023-11-16 14:49:16 -0700

    can't get this image thing to work... think I need to use modules maybe

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -22,7 +22,7 @@ async function getQuote(){
             var quoteContent = result[0].quote;
             var quoteAuthor = result[0].author;
             displayQuote(quoteContent, quoteAuthor);
-
+            getImage(quoteAuthor);
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
@@ -47,9 +47,50 @@ async function getQuote2(){
             var quoteContent = result[0].quote;
             var quoteAuthor = result[0].author;
             displayQuote(quoteContent, quoteAuthor);
+            getImage(quoteAuthor);
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     });
+}
+
+function getImage(quoteAuthor) {
+    quoteAuthor = quoteAuthor + " portrait";
+    quoteAuthor = takeSpaces(quoteAuthor).toLowerCase();
+    console.log('author search: ' + quoteAuthor);
+    var url = 'https://api.bing.microsoft.com/v7.0/images/search/q=';
+    url += quoteAuthor;
+    
+    // $(document).ready(function(){
+    //     $.getJSON(url,
+    //     {
+    //         tags: quoteAuthor,
+    //         tagmode: "any",
+    //         format: "json"
+    //     },
+    //     function(data) {
+    //         var image_src = data.items[0]['media']['m'].replace("_m", "_b");
+
+    //         $('body').css('background-image', "url('" + image_src + "')");
+
+    //     });
+    // });
+}
+
+function takeSpaces(string) {
+    for(var i = 0; i < string.length; i++) {
+        if(string[i] == ' ' || string[i] == '.') {
+             string = replaceChar(string, "-", i);
+        }
+    }
+    return string;
+}
+
+function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+      
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
 }
